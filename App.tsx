596ca736430e874cd5051a8401ac1a3522518a82
Brendan Dileo <124395674+devDrendos@users.noreply.github.com>
2025-05-20 16:59:30 -0400

    Update app

--- App.tsx
+++ App.tsx
@@ -8,11 +8,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+import Home from './screens/Home';
+
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Home />
     </View>
   );
 }
