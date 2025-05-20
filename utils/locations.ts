46edf062c03591f80861fc9b3f7a0a01fcfc3ea0
Brendan Dileo <124395674+devDrendos@users.noreply.github.com>
2025-05-20 18:06:36 -0400

    Create locations data file

---
+++ utils/locations.ts
@@ -0,0 +1,16 @@
+
+/**
+ * @file locations.ts
+ * @author Brendan Dileo, May 2025
+ */
+
+
+export interface Location {
+    LATITUDE: number;
+    LONGITUDE: number;
+    NAME: string;
+    CATEGORY: string;
+    WEBSITE: string;
+    COMMENTS: string;
+}
+
