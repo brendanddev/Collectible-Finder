c97d5d63aaf104275f094eeccbc14f344fe74844
Brendan Dileo <124395674+devDrendos@users.noreply.github.com>
2025-05-22 18:43:50 -0400

    Add pick image button styles on capture screen

--- styles/captureStyles.ts
+++ styles/captureStyles.ts
@@ -241,6 +241,28 @@ const captureStyles = StyleSheet.create({
     fontSize: 12,
     fontWeight: "600",
   },
+
+    pickImageButton: {
+    marginTop: 15,
+    backgroundColor: "#70a1b3",
+    paddingVertical: 12,
+    paddingHorizontal: 20,
+    borderRadius: 25,
+    elevation: 3,
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.3,
+    shadowRadius: 4,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+
+  pickImageButtonText: {
+    color: "white",
+    fontSize: 16,
+    fontWeight: "600",
+    textAlign: "center",
+  },
 });
 
 export default captureStyles;
