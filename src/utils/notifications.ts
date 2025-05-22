14b04bc62ae7741eee2802918cc295efa5dfd449
Brendan Dileo <124395674+devDrendos@users.noreply.github.com>
2025-05-22 19:58:10 -0400

    Implement logic for requesting permissions

--- src/utils/notifications.ts
+++ src/utils/notifications.ts
@@ -6,6 +6,9 @@
  */
 
 import * as Notifications from 'expo-notifications';
+import * as Device from 'expo-device';
+
+import { Platform } from 'react-native';
 
 // Configure foreground notification behaviour
 Notifications.setNotificationHandler({
@@ -20,10 +23,41 @@ Notifications.setNotificationHandler({
 
 // Request notification permissions
 export async function requestNotificationPermissions(): Promise<string | undefined> {
-    // Check device type
-    // Check if permissions are already granted
-    // If not, request permissions
-    // Handle any response
+    let expoPushToken: string | undefined;
+
+    // Check if device is physical device
+    if (Device.isDevice) {
+        const { status: existingPermissionStatus } = await Notifications.getPermissionsAsync();
+        let currentPermissionStatus = existingPermissionStatus;
+
+        // Request if not granted already
+        if (existingPermissionStatus !== 'granted') {
+            const { status: newPermissionStatus } = await Notifications.requestPermissionsAsync();
+            currentPermissionStatus = newPermissionStatus;
+        }
+
+        // If permission granted get token
+        if (currentPermissionStatus === 'granted') {
+            const tokenResponse = await Notifications.getExpoPushTokenAsync();
+            expoPushToken = tokenResponse.data;
+            console.log('Expo Push Token:', expoPushToken);
+        } else {
+            console.warn('Notification permissions not granted');
+        }
+    } else {
+        console.warn('Must use physical device for Push Notifications');
+    }
+
+    if (Platform.OS === 'android') {
+
+        await Notifications.setNotificationChannelAsync('default', {
+            name: 'default',
+            importance: Notifications.AndroidImportance.DEFAULT,
+            sound: 'default',
+        });
+    }
+
+    return expoPushToken;
 }
 
 // Send basic noti 
