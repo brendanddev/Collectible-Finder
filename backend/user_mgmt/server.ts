6ec146f33a1a120404cda74d74f8eb0dff38af96
Brendan Dileo <124395674+devDrendos@users.noreply.github.com>
2025-05-22 20:41:23 -0400

    Setup basic backend server logic

---
+++ backend/user_mgmt/server.ts
@@ -0,0 +1,18 @@
+
+/**
+ * @file server.ts
+ * @author Brendan Dileo, May 2025
+ */
+
+import express from 'express';
+
+const app = express();
+const PORT = 3000;
+
+app.get('/', (_req, res) => {
+  res.send('Hello!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
