07e0f430a1cd022b99b7db15f9c07f281bbfea9d
Brendan Dileo <124395674+devDrendos@users.noreply.github.com>
2025-05-24 13:10:02 -0400

    Add basic screen structure for login and create account

--- src/screens/CreateAccount.tsx
+++ src/screens/CreateAccount.tsx
@@ -5,7 +5,51 @@
  * The create account page for the app
  */
 
+import { SafeAreaView, Text, TextInput, View, TouchableOpacity, Alert } from 'react-native'
+import { useState } from 'react';
+import createAccountStyles from "../styles/createAccountStyles";
+
 const CreateAccount = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
+
+    const handleCreateAccount = async () => {
+    };
+
+    return (
+        <SafeAreaView>
+            <Text>Create Account</Text>
+
+            <TextInput
+                placeholder="Email"
+                value={email}
+                onChangeText={setEmail}
+                keyboardType="email-address"
+                autoCapitalize="none"
+            />
+            <TextInput
+                placeholder="Password"
+                value={password}
+                onChangeText={setPassword}
+                secureTextEntry
+            />
+            <TextInput
+                placeholder="Confirm Password"
+                value={confirmPassword}
+                onChangeText={setConfirmPassword}
+                secureTextEntry
+            />
+
+            <TouchableOpacity onPress={handleCreateAccount}>
+                <Text>Sign Up</Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity>
+                <Text>Already have an account? Log in</Text>
+            </TouchableOpacity>
+        </SafeAreaView>
+    );
 }
 
 export default CreateAccount;
--- src/screens/Login.tsx
+++ src/screens/Login.tsx
@@ -5,8 +5,56 @@
  * The login screen conditionally rendered for the app
  */
 
+import { SafeAreaView, View, ScrollView, Text, TouchableOpacity, Alert, TextInput } from "react-native"
+import { useState } from 'react';
+import { useAuth } from '../contexts/AuthContext';
+import loginStyles from "../styles/loginStyles"
+
 
 const Login = () => {
-}
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const { login } = useAuth();
+
+    const handleLogin = async () => {
+        if (!email || !password) return Alert.alert("Error", "All fields are required!");
+
+        try {
+            await login(email, password);
+        } catch (error) {
+            Alert.alert("Login Failed", "Invalid email or password!");
+        }
+    };
+
+
+    return (
+        <SafeAreaView>
+            <Text>Login</Text>
+
+            <TextInput
+                placeholder="Email"
+                value={email}
+                onChangeText={setEmail}
+                keyboardType="email-address"
+                autoCapitalize="none"
+            />
+
+            <TextInput
+                placeholder="Password"
+                value={password}
+                onChangeText={setPassword}
+                secureTextEntry
+            />
+
+            <TouchableOpacity onPress={handleLogin}>
+                <Text>Login</Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity>
+                <Text>Don't Have an Account? Create one!</Text>
+            </TouchableOpacity>
+        </SafeAreaView>
+    );
+};
 
 export default Login;
