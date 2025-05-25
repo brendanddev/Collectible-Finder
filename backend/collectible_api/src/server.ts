7dd6d7aa8ebde685e2a41b448c24785f1b2724d2
Brendan Dileo <124395674+devDrendos@users.noreply.github.com>
2025-05-25 11:39:09 -0400

    Setup basic server file

--- backend/collectible_api/src/server.ts
+++ backend/collectible_api/src/server.ts
@@ -4,3 +4,19 @@
  * @author Brendan Dileo, May 2025
  * 
  */
+
+import express from 'express';
+import cors from 'cors';
+import Database from 'better-sqlite3';
+
+// App setup
+const app = express();
+const PORT = 3001;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
