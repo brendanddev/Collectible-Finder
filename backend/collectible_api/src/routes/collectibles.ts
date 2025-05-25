c479e4518bf259c509695c6aa33cdab8de1748db
Brendan Dileo <124395674+devDrendos@users.noreply.github.com>
2025-05-25 11:48:36 -0400

    Basic route outline

--- backend/collectible_api/src/routes/collectibles.ts
+++ backend/collectible_api/src/routes/collectibles.ts
@@ -4,3 +4,20 @@
  * @author Brendan Dileo, May 2025
  * 
  */
+
+import express from 'express';
+
+const router = express.Router();
+
+// Get all locations
+router.get('/collectible_locations/', (req, res) => {
+});
+
+// Get a specific location
+router.get('/collectible_locations/:name', (req, res) => {
+});
+
+// Add a new location?
+router.post('/collectible_locations', (req, res) => {
+});
+
