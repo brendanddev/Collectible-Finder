71d327564395f724b52559344d5195d390cf685c
Brendan Dileo <124395674+devDrendos@users.noreply.github.com>
2025-05-25 12:02:02 -0400

    Add global styles file

---
+++ src/styles/globalStyles.ts
@@ -0,0 +1,14 @@
+
+
+/**
+ * @file globalStyles.ts
+ * @author Brendan Dileo, May 2025
+ * Global styles to be applied accross the app.
+ */
+
+import { StyleSheet } from 'react-native';
+
+const globalStyles = StyleSheet.create({
+});
+
+export default globalStyles;
